464b5df3e071eb440258b8528b7f774120339451
Alex Entwistle <alexentwistleseo@gmail.com>
2019-01-30 11:41:16 +0000

    Added comments on home404

--- home404.py
+++ home404.py
@@ -22,6 +22,10 @@ url = domain
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page,'html.parser')
 
+
+# TODO extract the href attributes from the link <a> elements.
 link_finder = soup.find('a','href')
 
 print(link_finder)
+
+# TODO include a delay to avoid overloading the server
