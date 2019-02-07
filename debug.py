346b69fc0aaa3cb79b1e45c6310fc0d7666e9084
Alex Entwistle <alexentwistleseo@gmail.com>
2019-02-07 15:59:32 +0000

    Created function for ease of checking URLs

--- debug.py
+++ debug.py
@@ -10,13 +10,21 @@ working  = "http://www.lloydspharmacy.com/en/info/alpecin"
 
 broken = "http://www.lloydspharmacy.com/en/info/alpecin404"
 
+
+# Define the function 
+def check(url):
+	print(urllib.request.urlopen(url).getcode())
+
+
+
+
 # Check response code of working address
 print(working)
-print(urllib.request.urlopen(working).getcode())
+check(working)
 
 
 
 # Check response code of broken address
 # TODO Fix this. It still identifies the broken address as a 404, but in an ugly, broken-looking way.
 print(broken)
-print(urllib.request.urlopen(broken).getcode())
+check(broken)
